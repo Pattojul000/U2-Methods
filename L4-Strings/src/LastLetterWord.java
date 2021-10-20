9cb4976212ce5ddade0a1e3985ebd395e42041a6
pattojul000 <pattojul000@warren.k12.in.us>
2021-10-20 11:42:42 -0400

    U2 Commits

--- L4-Strings/src/LastLetterWord.java
+++ L4-Strings/src/LastLetterWord.java
@@ -1,3 +1,5 @@
+import javax.swing.*;
+
 public class LastLetterWord {
 
     /*
@@ -7,12 +9,32 @@ public class LastLetterWord {
     Use a method to find and return the last three letters and a string parameter.
 
     Ex:  Goodbye, Hello, Morning -> byelloing
+
+
+    The Plan:
+           1. Get input: three words input
+           2. Isolate the last three letters of any word
+           3.Combine #2 of all three words
+
+
+
     */
 
     public static void main(String[] args) {
 
+        String word1 = JOptionPane.showInputDialog("Enter word 1: ");
+        String word2 = JOptionPane.showInputDialog("Enter word 2: ");
+        String word3 = JOptionPane.showInputDialog("Enter word 3: ");
 
+        String output = lastThree(word1) + lastThree(word2) + lastThree(word3);
+
+        JOptionPane.showMessageDialog(null, output);
 
     }
 
+
+    public static String lastThree(String word){
+
+        return word.substring(word.length()-3);
+    }
 }
