175f74fa84890759f8c5e4d1d3ab0c1f17afee57
pattojul000 <pattojul000@warren.k12.in.us>
2021-10-27 11:43:45 -0400

    U2 Commits

--- L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
+++ L7-Swing Colors and Paint/src/SimpleDrawingGUI.java
@@ -7,13 +7,14 @@ public class SimpleDrawingGUI {
 
     public SimpleDrawingGUI(){
         frame = new JFrame("Simple Drawing");
-        frame.setSize(400, 600);
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
 
         //ADD THE PANEL TO THE FRAME
+        JPanel panel = new DrawingPanel();
 
-
+        frame.add(panel);
         frame.setVisible(true);
     }
 
@@ -22,9 +23,39 @@ public class SimpleDrawingGUI {
         //Constructor for the class
         public DrawingPanel(){
             //set the Background color
+            setBackground(Color.blue);
 
         }
 
 
+
+        public void paintComponent(Graphics g){
+
+            super.paintComponent(g);
+
+            g.setColor(Color.RED);
+            g.drawRect(125, 215, 250, 200);
+
+            Color grass = new Color(123, 252, 121);
+            g.setColor(grass);
+            g.fillRect(0, 416, 500, 500);
+
+            g.setColor(Color.GREEN);
+            g.fillRect(225, 345, 45, 70);
+
+            Color teal = new Color(0, 128, 128);
+            g.setColor(teal);
+            g.drawLine(125, 215, 250, 40);
+
+            g.setColor(teal);
+            g.drawLine(375, 215, 250, 40);
+
+            Color pink = new Color(255, 196, 209);
+            g.setColor(pink);
+            g.fillArc(140,240,50,50,0,360);//Left Window
+            g.fillArc(310,240,50,50,0,360);//Right Window
+
+
+        }
     }
 }
